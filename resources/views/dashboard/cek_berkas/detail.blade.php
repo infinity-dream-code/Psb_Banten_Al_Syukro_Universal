8b3e7dd733f207cf152e58387faca64708aa97f6
infinity-dream-code <infinity-dream-code@gmail.com>
2025-09-24 13:06:37 +0700

    p

--- resources/views/dashboard/cek_berkas/detail.blade.php
+++ resources/views/dashboard/cek_berkas/detail.blade.php
@@ -1,14 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Calon Siswa - {{ $peserta->nama_peserta }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body class="bg-gray-100 min-h-screen">
     <div class="container mx-auto px-4 py-6">
         <div class="bg-white rounded-lg shadow-lg overflow-hidden">
             {{-- Header --}}
@@ -292,5 +282,3 @@
         }
     }
     </style>
-</body>
-</html>
