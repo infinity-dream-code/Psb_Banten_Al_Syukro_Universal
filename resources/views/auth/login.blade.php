e6b74ddfe9cd7da275dfa3153b3f9680db6e4db3
infinity-dream-code <infinity-dream-code@gmail.com>
2025-11-04 16:31:06 +0700

    p

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -259,13 +259,13 @@
           <p class="text-gray-400 text-sm mb-6">— or Another Links —</p>
 
           <div class="flex justify-center gap-4">
-            <a href="#" class="w-12 h-12 flex items-center justify-center rounded-full bg-blue-600 text-white shadow-lg icon-link">
+            <a href="{{url('/')}}" class="w-12 h-12 flex items-center justify-center rounded-full bg-blue-600 text-white shadow-lg icon-link">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
               </svg>
             </a>
             
-            <a href="#" class="w-12 h-12 flex items-center justify-center rounded-full bg-sky-400 text-white shadow-lg icon-link">
+            <a href="{{url('/enroll')}}" class="w-12 h-12 flex items-center justify-center rounded-full bg-sky-400 text-white shadow-lg icon-link">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
               </svg>
